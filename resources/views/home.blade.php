613ed8d6180e1588efce2dcfc4acb466bca16f0c
Mohammad Ahmady Yeganeh <myegane49@protonmail.com>
2020-10-24 19:16:27 +0330

    Adds the views for home and admin and single post and some primary routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,34 @@
-@extends('layouts.app')
+<x-home-master>
+    @section('content')
+      <h1 class="my-4">Page Heading
+        <small>Secondary Text</small>
+      </h1>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+      <!-- Blog Post -->
+      @foreach ($posts as $post)
+        <div class="card mb-4">
+          <img class="card-img-top" src="{{$post->post_image}}" alt="Card image cap">
+          <div class="card-body">
+            <h2 class="card-title">{{$post->title}}</h2>
+            <p class="card-text">{{Str::limit($post->body, '50', '...')}}</p>
+            <a href="{{route('post', $post->id)}}" class="btn btn-primary">Read More &rarr;</a>
+          </div>
+          <div class="card-footer text-muted">
+            Posted on {{$post->created_at->diffForHumans()}} by
+            <a href="#">Start Bootstrap</a>
+          </div>
         </div>
-    </div>
-</div>
-@endsection
+      @endforeach
+
+      <!-- Pagination -->
+      <ul class="pagination justify-content-center mb-4">
+        <li class="page-item">
+          <a class="page-link" href="#">&larr; Older</a>
+        </li>
+        <li class="page-item disabled">
+          <a class="page-link" href="#">Newer &rarr;</a>
+        </li>
+      </ul>
+    @endsection
+</x-home-master>
+
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
+Route::get('/post/{post}', [App\Http\Controllers\PostController::class, 'show'])->name('post');
